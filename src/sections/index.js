70459fdd0325b63561b0ba72378b77395822b6e3
Farhang-Osman <168280475+Farhang-Osman@users.noreply.github.com>
2024-05-03 20:02:26 +0100

    faet: add Components folder and index.js file to sections

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import CostumarReviews from './CostumarReviews';
+import Footer from './Footer';
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import Services from './Services';
+import SpecialOffers from './SpecialOffers';
+import Subscribe from './Subscribe';
+import SuperQuality from './SuperQuality';
+
+export {
+    CostumarReviews,
+    Footer,
+    Hero,
+    PopularProducts,
+    Services,
+    SpecialOffers,
+    Subscribe,
+    SuperQuality,
+}
