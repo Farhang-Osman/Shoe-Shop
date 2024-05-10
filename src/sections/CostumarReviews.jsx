a63745e641cec8b8f4e752b5fea5e46fc2459a90
Farhang-Osman <168280475+Farhang-Osman@users.noreply.github.com>
2024-05-10 16:07:37 +0100

    feat: add images and text to customerReview section

--- src/sections/CostumarReviews.jsx
+++ src/sections/CostumarReviews.jsx
@@ -1,5 +1,32 @@
+import ReviewCard from '../Components/ReviewCard';
+import { reviews } from '../constants';
+
 const CostumarReviews = () => {
-  return <div>CostumarReviews</div>;
+  return (
+    <section className='max-container'>
+      <h3 className='font-palanquin text-center text-4xl font-bold'>
+        what our <span className='text-coral-red'>Costumers</span> say ?
+      </h3>
+      <p className='info-text text-center m-auto mt-4 max-w-lg'>
+        Hear genuine stories from our satisfied customers about their
+        exceptional experiences with us.
+      </p>
+      <div
+        className='mt-24 flex flex-1 justify-evenly items-center
+        max-lg:flex-col gap-14'
+      >
+        {reviews.map((review, index) => (
+          <ReviewCard
+            key={index}
+            customerName={review.customerName}
+            imgURL={review.imgURL}
+            rating={review.rating}
+            feedback={review.feedback}
+          />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default CostumarReviews;
